72c51d98258d156dfe35069bef6825e1806eb2a8
Ibraheem Alyan <35265448+ibraheemalayan@users.noreply.github.com>
2023-05-21 18:58:08 +0300

    merge: 🔀 merge code parts

--- main.asm
+++ main.asm
@@ -35,6 +35,7 @@ dictionaryWriteFileDescriptor: .space 2 # descriptor is 2 bytes long ( half word
 mainBuffer: .space BUFFER_SIZE    # Space to store long input strings
 wordsArray: .space BUFFER_SIZE # Space to store the words ( dictionary ) as SEPARATOR separated strings
 secondaryBuffer: .space BUFFER_SIZE    # Space to store temporary data
+word: .space 128
 
 # #############################################################################
 # ################################## PROMPTS ##################################
@@ -51,11 +52,14 @@ promptQuitting: .asciiz "\nQuitting..."
 promptErrorReadingFile: .asciiz "\nError reading file, error code: "
 promptErrorFixingFilePath: .asciiz "\nError fixing file path"
 promptClosingFiles: .asciiz "\nClosing files..."
-
+compressedSizePrompt: .asciiz "\nCompressed file size: "
+uncompressedSizePrompt: .asciiz "\nUncompressed file size: "
+compressionRatioPrompt: .asciiz "\nCompression Ratio: "
 chooseOperationPrompt:     .asciiz "\nChoose the operation (C for compression, D for decompression, Q to quit): \n> "
 invalidChoiceMsg:  .asciiz "\nInvalid choice. Please try again.\n"
 compressMsg: .asciiz "\nEnter path of text file to compress: \n> "
 decompressMsg: .asciiz "\nDecompression function called.\n"
+sep: .asciiz "~"
 
 
 .text
@@ -216,63 +220,327 @@ incorrect_choice:
 
 # ############################## Main Functions #############################
 
-# Function ( returns word index in $v0, or -1 if not found )
-# $a0 : address of word to search for
-# uses t4, t5, t6, t7, t8, t9, s0
-FindWordInDictionary:
+# Function ( returns index in v0 )
+# $a0 = word address
+FindWordInwordsArray:
 
-    move $t5, $a0       # move word address to $t5
-    la $t6, wordsArray  # load dictionary address into $t6
+    addi $s3, $s3, 1 # increment number of compressed tokens
+
+    li $t0, 0       # i = 0
+    li $t1, 0       # j = 0
+    li $t2, 0       # word_index = 0
+
+    li $t3, 0       # go_to_next_word = False
+
+while_loop:
+    lb $t4, wordsArray($t0)     # dict_char = wordsArray[i]
+
+    add $t6, $a0, $t1    # word_ptr += t1
+    lb $t5, 0($t6)           # word_char = word[j]
+
+    bne $t4, $zero, check_go_to_next_word   # If dict_char != '\0', check_go_to_next_word
+
     
-    li $t7, 0           # initialize word index to 0
-    li $t8, 0           # initialize character index to 0
-
-    li $s0, '~'         # initialize $s0 to '~' ( delimiter )
-
-find_word_in_dictionary_loop:
-
-    lb $t9, ($t5)       # load character from word into $t9
-    lb $t4, ($t6)       # load character from dictionary into $t4
-
-    # if t9 != t4 then go to next word by looping over dictionary chatacters until $s0 is found
-    bne $t9, $t4, next_dictionary_character
+    beq $t5, $zero, return_word_found # if  word_char == '\0' return word_index
+    j return_not_found # else return -1
 
 
+check_go_to_next_word:
+    beq $t3, $zero, check_chars # If go_to_next_word == False, check characters
 
+    addi $t0, $t0, 1    # i += 1
+    bne $t4, SEPARATOR, while_loop # If dict_char != '~', continue
 
+    li $t1, 0 # j = 0
+    addi $t2, $t2, 1 # word_index += 1
+    li $t3, 0 # go_to_next_word = False
 
-next_dictionary_character:
-    addi $t6, $t6, 1
+    j while_loop
 
+check_chars:
 
+    # if dict_char == word_char:
+            # i += 1
+            # j += 1
+            # continue
+    bne $t4, $t5, check_end_of_dict_and_word  # If dict_char != word_char, check_end_of_dict_and_word
+    addi $t0, $t0, 1    # i += 1
+    addi $t1, $t1, 1    # j += 1
+    j while_loop
 
+check_end_of_dict_and_word:
 
-# character_match:
+    #  elif (
+            # dict_char == "~" or dict_char == "\0"
+        # ) and word_char == "\0":  # check_end_of_dict_and_word
+            # return word_index  # move word index to v0 and jr s7
     
-#     beqz $t9, word_found    # if $t9 == 0 then word_found
+    # if t5 != "\0" or (t4 != "~" and t4 != "\0"):
+    #     j check_end_of_dict
+    bne $t5, $zero, check_end_of_dict
 
-#     # if $t4 == $s0 == '~' then word_not_found
-#     beq $t4, $s0, word_not_found
+    # if (t4 != "~" and t4 != "\0"):
+    #     j check_end_of_dict
+    # else:
+    #     j word_and_dict_ended
     
-    
-#     bne $t9, $t4, word_not_found   # if $t9 != $t10 then word_not_found
-    
-#     addi $t5, $t5, 1    # increment word address
-#     addi $t6, $t6, 1    # increment dictionary address
-#     addi $t8, $t8, 1    # increment character index
-#     j find_word_in_dictionary_loop
+    beq $t4, SEPARATOR, word_and_dict_ended
+    bne $t4, $zero, check_end_of_dict
 
+word_and_dict_ended:
+    j return_word_found
 
+check_end_of_dict:
+
+    # if dict_char == "\0":
+    #       j return_not_found
+    beq $t4, $zero, return_not_found
+    
+check_end_of_dict_word:
+
+    # if dict_char == "~":
+        # i += 1
+        # j = 0
+        # word_index += 1
+        # continue 
+
+    # if t4 != "~":
+    #     j check_end_of_word
+
+    bne $t4, SEPARATOR, check_end_of_word
+
+    # else
+
+    addi $t0, $t0, 1    # i += 1
+    li $t1, 0           # j = 0
+    addi $t2, $t2, 1    # word_index += 1
+    j while_loop
+
+check_end_of_word:
+
+    # if word_char == "\0" or dict_char != word_char:  # check_end_of_word
+        # j = 0
+        # i += 1
+        # go_to_next_word = True
+        # continue  # j while_loop
+
+    beq $t5, $zero, end_of_word
+    bne $t4, $t5, end_of_word
+
+    j error_in_compresion
+    
+end_of_word:
+
+    li $t1, 0           # j = 0
+    addi $t0, $t0, 1    # i += 1
+    li $t3, 1           # go_to_next_word = True
+    j while_loop
+
+error_in_compresion:
+    li $v0, -2          # Return -2 (error)
+    j append_if_minus
+
+return_not_found:
+    li $v0, -1          # Return -1 (word not found)
+    j append_if_minus
+return_word_found:
+    move $v0, $t2       # Move word_index to $v0 (return value)
+    j append_if_minus
+
+append_if_minus:
+
+    # if return value is negative, append it to the end of the wordsArray
+    bge $v0, $zero, return_index
+append_word:
+
+    li $t5, SEPARATOR           # word[j] = delimeter
+    sb $t5, wordsArray($t0) # wordsArray[i] = word[j]
+
+    addi $t0, $t0, 1    # i += 1
+
+    li $t1, 0           # j = 0
+
+    addi $t2, $t2, 1    # word_index += 1
+    
+append_loop:
+
+    # while (word[j] != '\0'):
+    #     wordsArray[i] = word[j]
+    #     j += 1
+    
+    add $t6, $a0, $t1    # word_ptr += j
+    lb $t5, 0($t6)           # word_char = word[j]
+
+    beq $t5, $zero, return_index # If word_char == '\0', return index
+    sb $t5, wordsArray($t0) # wordsArray[i] = word[j]
+    addi $t0, $t0, 1    # i += 1
+    addi $t1, $t1, 1    # j += 1
+    j append_loop
+
+return_index:
+
+    move $v1, $t2       # Move return value to $v0
+    
+    # print index just for debugging
+    move $a0, $v1        # Move return value to $a0
+    li $v0, 1           # Print return value
+    syscall
+
+    la $a0, sep         # Move return value to $a0
+    li $v0, 4           # Print return value
+    syscall
+
+    jr $ra
+
+# Function
+# args: $a3 - input
+IsCharAlpha:
+
+    # Convert choice to uppercase
+    andi $a3, $a3, 0xDF
+
+    # if (input >= 'A' && input <= 'Z') return 1;
+    # eqv to
+    # if (input < 'A' || input > 'Z') return 0;
+    li $t8, 'A'
+    li $t9, 'Z'
+    bgt $a3, $t9, return_0
+    blt $a3, $t8, return_0
+
+    li $v0, 1
+    jr $ra
+
+return_0:
+    li $v0, 0
+    jr $ra
 
 
 # Function
-# No arguments
-CompressBuffer:
+# args: No args
+TokenizeText:
 
-    la $t0, secondaryBuffer # address of buffer containing the text to compress
-    la $t1, wordsArray # address of dictionary
+    move $s5, $ra
+    
+    li $s0, 0 # int textIndex = 0;
+    li $s1, -1 # int tokenStartIndex = -1;
+
+tokenize_text_loop:
+    
+    lb $s2, secondaryBuffer($s0) # char currentChar = text[textIndex];
+
+    #  if (currentChar == '\0') break;
+    beqz $s2, tokenize_text_end_loop
+
+    move $a3, $s2
+    jal IsCharAlpha
+
+    # if (!IsCharAlpha(currentChar)) j character_is_not_alpha
+    beqz $v0, character_is_not_alpha
+
+    li $t8, -1
+    bne $s1, $t8, next_iteration # if (tokenStartIndex != -1) j next_iteration
+
+    move $s1, $s0   # tokenStartIndex = textIndex;
+
+    j next_iteration
+
+character_is_not_alpha:
+
+    #  {
+        # text[textIndex] = '\0';
+        # handle_word(&text[tokenStartIndex]);
+        # tokenStartIndex = -1;
+    # }
+    
+    # if (tokenStartIndex == -1) j token_start_index_is_not_set
+    li $t8, -1
+    beq $s1, $t8, token_start_index_is_not_set 
+
+    # else
+    sb $zero, secondaryBuffer($s0) # text[textIndex] = '\0'
+
+    # handle_word(&text[tokenStartIndex]); # TODO
+    la $a0, secondaryBuffer
+    add $a0 , $a0, $s1
+    jal FindWordInwordsArray
+
+    li $s1, -1 # tokenStartIndex = -1;
 
 
+token_start_index_is_not_set:
+
+    sb $s2, word($zero) # word[0] = currentChar
+    li $t8, 1
+    sb $zero, word($t8) # word[1] = '\0'
+
+    # handle_word(&word); # TODO
+    la $a0, word
+    jal FindWordInwordsArray
+
+next_iteration:
+    addi $s0, $s0, 1 # textIndex++
+    j tokenize_text_loop
+
+tokenize_text_end_loop:
+
+    li $t8, -1
+    beq $s1, $t8, end_tokeniz_text # if (tokenStartIndex == -1) return
+
+    # handle_word(&text[tokenStartIndex]); # TODO
+    la $a0, secondaryBuffer
+    add $a0 , $a0, $s1
+    jal FindWordInwordsArray
+
+end_tokeniz_text:
+
+    # s0 contains chars of uncompressed file
+    # s3 contains chars of compressed file
+
+    # Print uncompressedSizePrompt
+    la $a0, uncompressedSizePrompt
+    li $v0, 4
+    syscall
+
+    # Print integer stored in $s0
+    move $a0, $s0
+    li $v0, 1
+    syscall
+
+    # Print compressedSizePrompt
+    la $a0, compressedSizePrompt
+    li $v0, 4
+    syscall
+
+    # Print integer stored in $s3
+    move $a0, $s3
+    li $v0, 1
+    syscall
+
+    # Print compressionRatioPrompt
+    la $a0, compressionRatioPrompt
+    li $v0, 4
+    syscall
+
+    mtc1 $s0, $f10
+    cvt.s.w $f10, $f10
+
+    mtc1 $s3, $f11
+    cvt.s.w $f11, $f11
+
+    # Calculate compression ratio
+    div.s $f12, $f10, $f11
+
+    # Print float stored in $f12
+    li $v0, 2
+    syscall
+
+    # print words array
+    la $a0, wordsArray
+    li $v0, 4
+    syscall
+
+
+    jr $s5    # Return to the caller
 
 
 # ############################### Menu Options ##############################
@@ -292,6 +560,19 @@ Compression:
     la $a1, secondaryBuffer # address of input buffer
     jal ReadFileIntoAddress # reads the file into secondaryBuffer ( and prints it )
 
+    # s3 contains number of tokens compressed
+    li $s3, 0
+
+    # Call tokeniz_text function
+    la $a0, secondaryBuffer
+    jal TokenizeText
+
+    # Exit program
+    li $v0, 10
+    syscall
+
+
+    
     # TODO: compression function
 
     # end program
